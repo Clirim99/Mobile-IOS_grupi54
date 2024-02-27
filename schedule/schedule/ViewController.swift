9b0095082795da9eaf879a822f40697a2aa6099e
Clirim99 <117774775+Clirim99@users.noreply.github.com>
2024-02-27 21:44:29 +0100

    main layouts
    log in, sign up and main view.

---
+++ schedule/schedule/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  schedule
+//
+//  Created by user257547 on 2/26/24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+
+        
+    }
+
+
+}
