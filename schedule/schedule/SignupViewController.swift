77e57eaa462a2333d6b4b06beb2f5b5a246d13cd
Clirim99 <117774775+Clirim99@users.noreply.github.com>
2024-02-28 00:38:48 +0100

    Database_added

---
+++ schedule/schedule/SignupViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SignupViewController.swift
+//  schedule
+//
+//  Created by user257547 on 2/27/24.
+//
+
+import UIKit
+import SQLite3
+
+class SignupViewController: UIViewController {
+    @IBOutlet var first_name:UITextField!
+    @IBOutlet var last_name:UITextField!
+    @IBOutlet var email:UITextField!
+    @IBOutlet var username:UITextField!
+    @IBOutlet var password:UITextField!
+    @IBOutlet var cofirmpassword:UITextField!
+    @IBOutlet var signupButton: UIButton!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signupaction(sender: UIButton) {
+            // 1. Collect the data from the text fields
+                
+            }
+            
+          
+        
+        
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
